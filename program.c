1860a3763e303def3425daae33c292e5353de09a
Arjun Vasavan <vasavanarjun@gmail.com>
2026-01-09 12:19:43 +0530

    assign 14

---
+++ program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct {
+
+    int start;
+    int end;
+    int* array;
+
+} container;
+
+int main(int argc, char** argv) {
+    
+    int n;
+    printf("Enter the size of the array : ");
+    scanf("%d",&n);
+
+}
